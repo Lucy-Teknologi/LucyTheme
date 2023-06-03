0b7cdf2ef8a24a0581f2bc5f666570e2150401e5
Gilbert Thiodores <gthiodores71@gmail.com>
2023-06-03 13:26:35 +0800

    [Misc] Added more illustrations

--- lib/src/main/java/app/lucys/lib/lucytheme/ui/atom/icon/LucyIllustration.kt
+++ lib/src/main/java/app/lucys/lib/lucytheme/ui/atom/icon/LucyIllustration.kt
@@ -6,8 +6,14 @@ import app.lucys.lib.lucytheme.R
 enum class LucyIllustration(@DrawableRes val resourceId: Int) {
     EmailCompleted(R.drawable.email_completed),
     ErrorOccurred(R.drawable.error_occurred),
+
     Welcome(R.drawable.welcome),
     GoodBye(R.drawable.goodbye),
+
     RippleTopEnd(R.drawable.ripple_top_end),
-    RippleBottomStart(R.drawable.ripple_bottom_start)
+    RippleBottomStart(R.drawable.ripple_bottom_start),
+
+    Communication(R.drawable.illust_communication),
+    SocialMedia(R.drawable.illust_socialmedia),
+    Workflow(R.drawable.illust_workflow)
 }
