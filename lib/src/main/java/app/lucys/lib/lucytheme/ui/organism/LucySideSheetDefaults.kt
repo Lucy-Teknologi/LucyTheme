9a03373190d19e3f96d1af9a549736b82f344b2c
Gilbert Thiodores <gthiodores71@gmail.com>
2023-11-16 16:06:01 +0800

    feat: added lucy side sheet

---
+++ lib/src/main/java/app/lucys/lib/lucytheme/ui/organism/LucySideSheetDefaults.kt
@@ -0,0 +1,18 @@
+package app.lucys.lib.lucytheme.ui.organism
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import app.lucys.lib.lucytheme.ui.theme.Neutral60
+
+object LucySideSheetDefaults {
+    val ContentPadding = PaddingValues(top = 24.dp, start = 24.dp, end = 24.dp)
+    val HeaderItemSpacing = 12.dp
+    val HeaderPadding = 12.dp
+
+    val BottomActionHeight = 72.dp
+    val MaxWidth = 296.dp
+
+    val DividerColor: Color = Neutral60
+    val DividerWidth = 1.dp
+}
