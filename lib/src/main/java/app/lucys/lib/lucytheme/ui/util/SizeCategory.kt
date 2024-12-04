f90f169b1bae597fd04db80cb1f8494215bfb445
Gilbert Thiodores <gthiodores71@gmail.com>
2024-12-04 14:34:35 +0800

    update: change impl of size category

--- lib/src/main/java/app/lucys/lib/lucytheme/ui/util/SizeCategory.kt
+++ lib/src/main/java/app/lucys/lib/lucytheme/ui/util/SizeCategory.kt
@@ -1,14 +1,17 @@
 package app.lucys.lib.lucytheme.ui.util
 
-import androidx.compose.material3.windowsizeclass.WindowHeightSizeClass
-import androidx.compose.material3.windowsizeclass.WindowSizeClass
-import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
+import androidx.compose.material3.adaptive.WindowAdaptiveInfo
+import androidx.compose.material3.adaptive.currentWindowAdaptiveInfo
+import androidx.compose.runtime.Composable
+import androidx.window.core.layout.WindowHeightSizeClass
+import androidx.window.core.layout.WindowSizeClass
+import androidx.window.core.layout.WindowWidthSizeClass
 
 sealed interface SizeCategory {
-    object MobilePortrait : SizeCategory
-    object MobileLandscape : SizeCategory
-    object Tablet : SizeCategory
-    object Expanded : SizeCategory
+    data object MobilePortrait : SizeCategory
+    data object MobileLandscape : SizeCategory
+    data object Tablet : SizeCategory
+    data object Expanded : SizeCategory
 
     fun isMobile(): Boolean {
         return this == MobilePortrait || this == MobileLandscape
@@ -16,24 +19,31 @@ sealed interface SizeCategory {
 
     companion object {
         fun calculate(size: WindowSizeClass): SizeCategory {
-            val widthClass = size.widthSizeClass
-            val heightClass = size.heightSizeClass
+            val widthClass = size.windowWidthSizeClass
+            val heightClass = size.windowHeightSizeClass
 
             if (
-                widthClass == WindowWidthSizeClass.Compact &&
-                heightClass != WindowHeightSizeClass.Compact
+                widthClass == WindowWidthSizeClass.COMPACT &&
+                heightClass != WindowHeightSizeClass.COMPACT
             ) {
                 return MobilePortrait
             }
 
-            if (heightClass == WindowHeightSizeClass.Compact) {
+            if (heightClass == WindowHeightSizeClass.COMPACT) {
                 return MobileLandscape
             }
 
             return when (widthClass) {
-                WindowWidthSizeClass.Medium -> Tablet
+                WindowWidthSizeClass.MEDIUM -> Tablet
                 else -> Expanded
             }
         }
     }
+}
+
+@Composable
+fun currentSizeCategory() = currentWindowAdaptiveInfo().sizeCategory()
+
+fun WindowAdaptiveInfo.sizeCategory(): SizeCategory {
+    return SizeCategory.calculate(this.windowSizeClass)
 }
