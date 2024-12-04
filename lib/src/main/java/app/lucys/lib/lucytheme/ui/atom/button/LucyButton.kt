c1f16b108fa5b7cf3711d3559dfb4b9b81713dbd
Gilbert Thiodores <gthiodores71@gmail.com>
2024-12-04 14:35:15 +0800

    misc: preview for button

--- lib/src/main/java/app/lucys/lib/lucytheme/ui/atom/button/LucyButton.kt
+++ lib/src/main/java/app/lucys/lib/lucytheme/ui/atom/button/LucyButton.kt
@@ -10,11 +10,13 @@ import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ButtonElevation
 import androidx.compose.material3.ProvideTextStyle
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
 import app.lucys.lib.lucytheme.ui.theme.LucyTheme
 
 /**
@@ -209,4 +211,14 @@ fun LucyTextButton(
         interactionSource = interactionSource,
         content = content
     )
+}
+
+@Preview
+@Composable
+private fun LucyTextButtonPreview() {
+    LucyTheme {
+        LucyTextButton(onClick = {}) {
+            Text("My Content")
+        }
+    }
 }
